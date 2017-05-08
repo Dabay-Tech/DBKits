b922e3570b3827238dd79c968d9223f7aa55b50b
Donkey-Tao <taofei0610@gmail.com>
2017-05-08 09:23:50 +0800

    version 1.0.0

---
+++ DBKit/DBUIKit/DBTextField.h
@@ -0,0 +1,13 @@
+//
+//  DBTextField.h
+
+//
+//  Created by Donkey-Tao on 15/5/31.
+//  Copyright © 2015年 http://taofei.me All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DBTextField : UITextField
+
+@end
---
+++ DBKit/DBUIKit/DBVerticalButton.h
@@ -0,0 +1,14 @@
+//
+//  DBVerticalButton.h
+//
+//  Created by Donkey-Tao on 15/5/31.
+//  Copyright © 2015年 http://taofei.me All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DBVerticalButton : UIButton
+
+-(void)db_setupHeaderViewButton:(NSString *)image selectedImage:(NSString *)selectedImage titleName:(NSString *)titleName;
+
+@end
