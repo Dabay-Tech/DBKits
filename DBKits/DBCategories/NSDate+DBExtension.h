15e3e5e33f4fc99b8a810a4fd37728c1c96592d3
Daonkey-Tao <taofei0610@gmail.com>
2017-05-25 09:58:54 +0800

    add notice

--- DBKits/DBCategories/NSDate+DBExtension.h
+++ DBKits/DBCategories/NSDate+DBExtension.h
@@ -10,28 +10,40 @@
 @interface NSDate (DBExtension)
 
 
+
+
 /**
- *比较from和self的时间差
+ 比较from和self的时间差
+
+ @param from 开始日期
+ @return 返回比较结果
  */
 -(NSDateComponents *)db_deltaFrom:(NSDate *)from;
 
 
+
 /**
- *判断是否是今年
+ 判断是否是今年
+
+ @return 返回是否为今年
  */
 -(BOOL)db_isThisYear;
 
 
-
 /**
- *判断是否是今天
+ 判断是否为今天
+
+ @return 返回结果是否为今天
  */
 -(BOOL)db_isToday;
 
 
 
+
 /**
- *判断是否是昨天
+ 判断是否是昨天
+
+ @return 返回BOOL类型的是否为昨天
  */
 -(BOOL)db_isYesterday;
 
